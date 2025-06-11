377c7c3eb39b7dd66a1845aa21bbcd76926b1757
SimónRojas <srojaspereira@frba.utn.edu.ar>
2025-06-11 14:36:07 -0300

    Agrego los cambios de contribuyente e identidad contribuyente

--- fuenteDinamica/src/main/java/domain/usuarios/Contribuyente.java
+++ fuenteDinamica/src/main/java/domain/usuarios/Contribuyente.java
@@ -1,19 +1,23 @@
 package domain.usuarios;
 
+import domain.solicitudes.SolicitudEliminacion;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
 
 //CONTRIBUYENTE
 public class Contribuyente {
     private String contribuyente_id;
     private Boolean es_administrador;
-    private List<IdentidadContribuyente> identidades ;
+    private List<IdentidadContribuyente> identidades;
+    private List<SolicitudEliminacion> solicitudes_eliminacion;
 
     public Contribuyente(String contribuyente_id, Boolean es_administrador) {
         this.contribuyente_id = contribuyente_id;
         this.es_administrador = es_administrador;
         this.identidades = new ArrayList<>();
+        this.solicitudes_eliminacion = new ArrayList<>();
     }
 
     public void modificarIdentidad(String nombre, String apellido, LocalDate fecha_nacimiento) {
@@ -31,5 +35,9 @@ public class Contribuyente {
 
     public void agregarIdentidad(IdentidadContribuyente identidad){
         this.identidades.add(identidad);
+    }
+
+    public void agregarSolicitudEliminacion(SolicitudEliminacion solicitud) {
+        this.solicitudes_eliminacion.add(solicitud);
     }
 }
--- fuenteEstatica/src/main/java/domain/usuarios/IdentidadContribuyente.java
+++ fuenteEstatica/src/main/java/domain/usuarios/IdentidadContribuyente.java
@@ -1,15 +1,18 @@
 package domain.usuarios;
 
+import domain.hechos.Hecho;
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.List;
 
 
 // IDENTIDAD CONTRIBUYENTE
-public class IdentidadContribuyente{
+public class IdentidadContribuyente {
     private String nombre;
     private String apellido;
     private LocalDate fecha_nacimiento;
     private Contribuyente contribuyente;
+    private List<Hecho> hechos_contribuidos;
 
     public IdentidadContribuyente(String nombre, String apellido, LocalDate fecha_nacimiento,  Contribuyente contribuyente){
         this.nombre = nombre;
@@ -21,4 +24,6 @@ public class IdentidadContribuyente{
     public Integer getEdad() {
         return Period.between(fecha_nacimiento, LocalDate.now()).getYears();
     }
+
+    public void agregarHechoContrubuido(Hecho hecho) { this.hechos_contribuidos.add(hecho); }
 }
