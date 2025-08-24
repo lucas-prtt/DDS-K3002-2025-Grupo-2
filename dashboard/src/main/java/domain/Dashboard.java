4edfe486f26f3739474bccf46decab176b6b40f7
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-23 23:06:45 -0300

    Anduvo?

--- dashboard/src/main/java/domain/Dashboard.java
+++ dashboard/src/main/java/domain/Dashboard.java
@@ -1,5 +1,6 @@
 package domain;
 
+
 import domain.connectionManager.ConnectionManager;
 import domain.menu.MenuPrincipal;
 import org.springframework.boot.SpringApplication;
@@ -7,11 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Dashboard {
-     public static void main(String[] args) {
-         SpringApplication.run(Dashboard.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(Dashboard.class, args);
         ConnectionManager.getInstance().registrarLocalHost(8084, "Agregador");
         ConnectionManager.getInstance().registrarLocalHost(8082, "Dinamica");
-         new MenuPrincipal().abrirMenu();
-         System.exit(0);
+        new MenuPrincipal().abrirMenu();
+        System.exit(0);
     }
 }
