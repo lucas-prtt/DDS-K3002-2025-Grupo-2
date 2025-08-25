e6dcf36847771e8dedbdadfd423ba57702ee2c82
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-25 10:29:15 -0300

    Post solicitudes en dashboard

---
+++ dashboard/src/main/java/domain/DTOs/ContribuyenteSolicitudDTO.java
@@ -0,0 +1,11 @@
+package domain.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor
+public class ContribuyenteSolicitudDTO {
+    Integer contribuyenteID;
+    //TODO: Va ultimaIdentidad y esAdministrador?
+}
--- dashboard/src/main/java/domain/menu/MenuAgregador.java
+++ dashboard/src/main/java/domain/menu/MenuAgregador.java
@@ -11,11 +11,12 @@ public class MenuAgregador extends Menu{
         System.out.println(ConnectionManager.getInstance().getServidorLocal("Publica"));
         System.out.println("Elija la opcion:");
         System.out.println("0. Salir");
-        System.out.println("1. Enviar Post Coleccion");
-        System.out.println("2. Enviar Post FuenteDinamica");
-        System.out.println("3. Enviar Get Coleccion curada");
-        System.out.println("4. Enviar Get Coleccion irrestricta");
-        System.out.println("5. Enviar Get Colecciones");
+        System.out.println("1. Enviar Post Coleccion (api administrativa)");
+        System.out.println("2. Enviar Post FuenteDinamica (api administrativa)");
+        System.out.println("3. Enviar Get Coleccion curada (api publica)");
+        System.out.println("4. Enviar Get Coleccion irrestricta (api publica)");
+        System.out.println("5. Enviar Get Colecciones (api publica)");
+        System.out.println("6. Enviar Post Solicitud de eliminacion (api publica)");
     }
 
     @Override
@@ -37,6 +38,9 @@ public class MenuAgregador extends Menu{
                 break;
             case 5:
                 SubMenuGetColecciones.abrirMenu();
+                break;
+            case 6:
+                SubMenuPostSolicitud.abrirMenu();
                 break;
         }
         return false;
