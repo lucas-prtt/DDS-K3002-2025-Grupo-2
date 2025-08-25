88f33d10a4fb56a6b6d3830d17b35b06ba72a2df
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-25 09:36:50 -0300

    Agrego try catch en SubMenuPostContribuyente

--- dashboard/src/main/java/domain/subMenu/SubMenuPostContribuyente.java
+++ dashboard/src/main/java/domain/subMenu/SubMenuPostContribuyente.java
@@ -11,8 +11,14 @@ public class SubMenuPostContribuyente {
     public static void abrirMenu(){
         System.out.println("Se enviara un post de contribuyente a fuenteDinamica.");
         System.out.println("Ingrese si el usuario es Aministrador (True o False)");
+        System.out.println("Default: false");
         Scanner scanner = new Scanner(System.in);
-        PostContribuyenteDTO contribuyenteDTO = new PostContribuyenteDTO(Boolean.parseBoolean(scanner.nextLine()));
+        Boolean admin;
+        try{ admin = Boolean.parseBoolean(scanner.nextLine());} catch (Exception e){
+            System.out.println("No se entendio el valor ingresado. Se establecio el valor por defecto (false)");
+            admin = false;
+        }
+        PostContribuyenteDTO contribuyenteDTO = new PostContribuyenteDTO();
         Integer respuesta = ApiClient.postContribuyente(contribuyenteDTO, ConnectionManager.getInstance().getServidorLocal("Dinamica"));
         SubMenuPatchIdentidad.lastContribuyenteId = respuesta;
         System.out.println("Se recibio la respuesta: El contribuyente numero " + respuesta + " fue creado.");
