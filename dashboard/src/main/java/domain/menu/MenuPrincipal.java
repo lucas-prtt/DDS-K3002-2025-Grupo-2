c85e45af69d3063bad6b0cc7582186caf6760c97
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-27 12:51:15 -0300

    Separo menu agregador en menu api publica y api administrativa

--- dashboard/src/main/java/domain/menu/MenuPrincipal.java
+++ dashboard/src/main/java/domain/menu/MenuPrincipal.java
@@ -6,8 +6,9 @@ public class MenuPrincipal extends Menu{
     void mostrarTextoOpciones() {
         System.out.println("Que menu desea abrir?");
         System.out.println("0. Salir");
-        System.out.println("1. Menu agregador (api publica y administrativa)");
-        System.out.println("2. Menu fuente dinámica");
+        System.out.println("1. Menu API publica");
+        System.out.println("2. Menu API administrativa");
+        System.out.println("3. Menu fuente dinámica");
     }
 
     @Override
@@ -16,9 +17,12 @@ public class MenuPrincipal extends Menu{
             case 0:
                 return true;
             case 1:
-                new MenuAgregador().abrirMenu();
+                new MenuApiPublica().abrirMenu();
                 break;
             case 2:
+                new MenuApiAdministrativa().abrirMenu();
+                break;
+            case 3:
                 new MenuFuenteDinamica().abrirMenu();
                 break;
         }
