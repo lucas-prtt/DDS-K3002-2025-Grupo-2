9e58265b96a61e92238f1c469b8beef62687a52f
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-27 12:51:15 -0300

    Separo menu agregador en menu api publica y api administrativa

--- dashboard/src/main/java/domain/menu/MenuApiPublica.java
+++ dashboard/src/main/java/domain/menu/MenuApiPublica.java
@@ -1,23 +1,21 @@
 package domain.menu;
 
-import domain.subMenu.apiAdministrativa.SubMenuEnviarPostFuente;
+import domain.subMenu.apiAdministrativa.SubMenuEnviarPostFuenteDinamica;
 import domain.subMenu.apiAdministrativa.SubMenuPostColeccion;
 import domain.subMenu.apiPublica.SubMenuGetColecciones;
 import domain.subMenu.apiPublica.SubMenuGetHechosCurados;
 import domain.subMenu.apiPublica.SubMenuGetHechosIrrestrictos;
 import domain.subMenu.apiPublica.SubMenuPostSolicitud;
 
-public class MenuAgregador extends Menu{
+public class MenuApiPublica extends Menu{
     @Override
     void mostrarTextoOpciones() {
-        System.out.println("Elija la opcion:");
+        System.out.println("API Publica\nElija la opcion:");
         System.out.println("0. Salir");
-        System.out.println("1. Enviar Post Coleccion (api administrativa)");
-        System.out.println("2. Enviar Post FuenteDinamica (api administrativa)");
-        System.out.println("3. Enviar Get Coleccion curada (api publica)");
-        System.out.println("4. Enviar Get Coleccion irrestricta (api publica)");
-        System.out.println("5. Enviar Get Colecciones (api publica)");
-        System.out.println("6. Enviar Post Solicitud de eliminacion (api publica)");
+        System.out.println("1. Enviar Get Coleccion curada");
+        System.out.println("2. Enviar Get Coleccion irrestricta");
+        System.out.println("3. Enviar Get Colecciones");
+        System.out.println("4. Enviar Post Solicitud de eliminacion");
     }
 
     @Override
@@ -26,21 +24,15 @@ public class MenuAgregador extends Menu{
             case 0:
                 return true;
             case 1:
-                SubMenuPostColeccion.abrirMenu();
-                break;
-            case 2:
-                SubMenuEnviarPostFuente.abrirMenu();
-                break;
-            case 3:
                 new SubMenuGetHechosCurados().abrirMenu();
                 break;
-            case 4:
+            case 2:
                 new SubMenuGetHechosIrrestrictos().abrirMenu();
                 break;
-            case 5:
+            case 3:
                 SubMenuGetColecciones.abrirMenu();
                 break;
-            case 6:
+            case 4:
                 SubMenuPostSolicitud.abrirMenu();
                 break;
         }
