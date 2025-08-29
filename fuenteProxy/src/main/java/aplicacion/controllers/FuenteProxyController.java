5430f3febbd1f6ba1ad740771245e2dab2035bfd
AgustinHerzkovich <aherzkovich@frba.utn.edu.ar>
2025-08-28 23:31:21 -0300

    Arreglo endpoint de fuente proxy

--- fuenteProxy/src/main/java/aplicacion/controllers/FuenteProxyController.java
+++ fuenteProxy/src/main/java/aplicacion/controllers/FuenteProxyController.java
@@ -1,9 +1,11 @@
 package aplicacion.controllers;
 
-import aplicacion.domain.fuentesMetamapa.FuenteMetamapa;
+import aplicacion.domain.FuenteProxy;
+import aplicacion.services.excepciones.FuenteNoEncontradaException;
 import domain.hechos.Hecho;
 
 import aplicacion.services.FuenteProxyService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,8 +25,17 @@ public class FuenteProxyController {
         return fuenteProxyService.importarHechos();
     }
 
+    @GetMapping("/{id}/hechos")
+    public ResponseEntity<List<Hecho>> obtenerHechosDeFuente(@PathVariable("id") Long id){
+        try {
+            return ResponseEntity.ok(fuenteProxyService.importarHechosDeFuente(id));
+        } catch (FuenteNoEncontradaException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
     @PostMapping("/fuente")
-    public ResponseEntity<Void> guardarFuente(@RequestBody FuenteMetamapa fuente){
+    public ResponseEntity<Void> guardarFuente(@RequestBody FuenteProxy fuente){
         fuenteProxyService.guardarFuente(fuente);
         return ResponseEntity.ok().build();
     }
