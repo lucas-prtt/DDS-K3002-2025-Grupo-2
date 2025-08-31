4dd7e00c939ed9b255218491f0b2702dc67fe655
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-30 22:30:00 -0300

    Arreglo Api administrativa y publica mandando mal los headers
    Antes se enviaban ciertos headers dos veces cuando Springboot enviaba el mismo ResponseEntity que le llego del servidor
    
    Ahora se crea uno nuevo sin los headers pero manteniendo el body y el status code

---
+++ dashboard/src/main/java/domain/subMenu/fuenteDinamica/SubMenuEnviarPostFuenteDinamica.java
@@ -0,0 +1,11 @@
+package domain.subMenu.fuenteDinamica;
+
+import domain.apiClient.ApiClient;
+import domain.connectionManager.ConnectionManager;
+
+public class SubMenuEnviarPostFuenteDinamica {
+    public static void abrirMenu(){
+        System.out.println("Esta funcion fue deprecada. Volviendo al menu previo");
+        //System.out.println("ID de fuente creada: " + ApiClient.postFuenteDinamica(ConnectionManager.getInstance().getServidorLocal("Dinamica")));
+    }
+}
