08c472a86d66da87e1d2e3a6c42ce2a60d3c5a07
AgustinHerzkovich <aherzkovich@frba.utn.edu.ar>
2025-09-02 14:00:49 -0300

    Arreglo pequeño detalle de fuente proxy y completo caso de prueba

--- fuenteProxy/src/main/java/aplicacion/domain/fuentesDemo/Conexion.java
+++ fuenteProxy/src/main/java/aplicacion/domain/fuentesDemo/Conexion.java
@@ -1,4 +1,6 @@
 package aplicacion.domain.fuentesDemo;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jakarta.persistence.Embeddable;
 
 import java.time.LocalDateTime;
@@ -6,6 +8,10 @@ import java.util.Map;
 
 //CONEXION
 @Embeddable
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "tipo")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ConexionPrueba.class, name = "prueba")
+}) // Esto es para testear nomas
 public interface Conexion {
     Map<String,Object> siguienteHecho(String url, LocalDateTime fechaUltimaConsulta);
 }
