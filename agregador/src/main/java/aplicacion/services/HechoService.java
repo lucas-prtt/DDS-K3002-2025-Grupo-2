d77c4fffdc39772733bc679cdc245071fdf7be0a
AgustinHerzkovich <aherzkovich@frba.utn.edu.ar>
2025-09-04 18:09:49 -0300

    Descubro error de normalizador, solo se crea la primer categoria, y todos los hechos quedan asignados a la misma categoria

--- agregador/src/main/java/aplicacion/services/HechoService.java
+++ agregador/src/main/java/aplicacion/services/HechoService.java
@@ -13,10 +13,8 @@ import aplicacion.repositorios.RepositorioDeHechos;
 import aplicacion.repositorios.RepositorioDeHechosXFuente;
 import aplicacion.repositorios.RepositorioDeHechosXColeccion;
 import aplicacion.excepciones.HechoNoEncontradoException;
-import aplicacion.services.depurador.DepuradorDeHechos;
 import aplicacion.services.normalizador.NormalizadorDeHechos;
 import org.springframework.stereotype.Service;
-import aplicacion.dto.mappers.HechoOutputMapper;
 
 import java.util.List;
 import java.util.Map;
@@ -30,16 +28,14 @@ public class HechoService {
     private final RepositorioDeFuentesXColeccion repositorioDeFuentesXColeccion;
     private final HechoOutputMapper hechoOutputMapper;
     private final NormalizadorDeHechos normalizadorDeHechos;
-    private final DepuradorDeHechos depuradorDeHechos;
 
-    public HechoService(RepositorioDeHechos repositorioDeHechos, RepositorioDeHechosXFuente repositorioDeHechosXFuente, RepositorioDeHechosXColeccion repositorioDeHechosXColeccion, RepositorioDeFuentesXColeccion repositorioDeFuentesXColeccion, HechoOutputMapper hechoOutputMapper, NormalizadorDeHechos normalizadorDeHechos, DepuradorDeHechos depuradorDeHechos) {
+    public HechoService(RepositorioDeHechos repositorioDeHechos, RepositorioDeHechosXFuente repositorioDeHechosXFuente, RepositorioDeHechosXColeccion repositorioDeHechosXColeccion, RepositorioDeFuentesXColeccion repositorioDeFuentesXColeccion, HechoOutputMapper hechoOutputMapper, NormalizadorDeHechos normalizadorDeHechos) {
         this.repositorioDeHechos = repositorioDeHechos;
         this.repositorioDeHechosXFuente = repositorioDeHechosXFuente;
         this.repositorioDeHechosXColeccion = repositorioDeHechosXColeccion;
         this.repositorioDeFuentesXColeccion = repositorioDeFuentesXColeccion;
         this.hechoOutputMapper = hechoOutputMapper;
         this.normalizadorDeHechos = normalizadorDeHechos;
-        this.depuradorDeHechos = depuradorDeHechos;
     }
 
     public void guardarHechos(List<Hecho> hechos) {
