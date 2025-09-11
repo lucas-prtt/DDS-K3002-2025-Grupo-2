92097acabfaaa7e1e7a0fee0c42996da90b43305
AgustinHerzkovich <aherzkovich@frba.utn.edu.ar>
2025-09-11 17:52:36 -0300

    Agrego endpoint para la carga de hechos manual

--- agregador/src/main/java/aplicacion/controllers/HechoController.java
+++ agregador/src/main/java/aplicacion/controllers/HechoController.java
@@ -4,6 +4,7 @@ import aplicacion.domain.hechos.Hecho;
 import aplicacion.dto.input.HechoInputDTO;
 import aplicacion.dto.output.HechoOutputDTO;
 import aplicacion.services.HechoService;
+import aplicacion.services.schedulers.CargarHechosScheduler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,9 +14,11 @@ import java.util.List;
 @RequestMapping("/agregador")
 public class HechoController {
     private final HechoService hechoService;
+    private final CargarHechosScheduler cargarHechosScheduler;
 
-    public HechoController(HechoService hechoService) {
+    public HechoController(HechoService hechoService, CargarHechosScheduler cargarHechosScheduler) {
         this.hechoService = hechoService;
+        this.cargarHechosScheduler = cargarHechosScheduler;
     }
 
     @GetMapping("/hechos")
@@ -28,5 +31,11 @@ public class HechoController {
         HechoOutputDTO hecho = hechoService.agregarHecho(hechoInputDto);
         System.out.println("Hecho creado: " + hecho.getId());
         return ResponseEntity.ok(hecho);
+    }
+
+    @PostMapping("/cargarHechos")
+    public ResponseEntity<Void> cargarHechos() { // Endpoint para disparar la carga de hechos manualmente (si es necesario)
+        cargarHechosScheduler.cargarHechos();
+        return ResponseEntity.ok().build();
     }
 }
