8cf65351a8d6818a4398be78d51391d1727cdf1e
EzeSu1 <esu@frba.utn.edu.ar>
2025-09-18 16:10:57 -0300

    Cambios en Fuente Proxy con UUID

--- fuenteProxy/src/main/java/aplicacion/services/schedulers/PedirHechosScheduler.java
+++ fuenteProxy/src/main/java/aplicacion/services/schedulers/PedirHechosScheduler.java
@@ -15,12 +15,6 @@ public class PedirHechosScheduler {
     @Scheduled(initialDelay = 10000, fixedRate = 3600000) // En Fuente Demo pide los hechos y se los guarda, y en Fuente Metamapa no hace nada
     public void pedirHechos() {
         fuenteProxyService.pedirHechos();
-        // delegar logica a fuenteProxyService
-        /*
-        //String url = "https://mocki.io/v1/66ea9586-9ada-4bab-a974-58abbe005292";
-        //RestTemplate restTemplate = new RestTemplate();
-        List<Hecho> hechos = List.of(restTemplate.getForObject(endpointHechos, Hecho[].class));
-        return hechos;*/
     }
 }
 
