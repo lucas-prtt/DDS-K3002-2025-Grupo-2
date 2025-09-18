0ea3213e89e8ddfba73cf54a40c1ee33005aae65
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-09-18 18:24:19 -0300

    IDs

--- estadisticas/src/main/java/aplicacion/domain/facts/FactColeccion.java
+++ estadisticas/src/main/java/aplicacion/domain/facts/FactColeccion.java
@@ -1,7 +1,11 @@
 package aplicacion.domain.facts;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import aplicacion.domain.dimensiones.DimensionCategoria;
+import aplicacion.domain.dimensiones.DimensionColeccion;
+import aplicacion.domain.dimensiones.DimensionTiempo;
+import aplicacion.domain.dimensiones.DimensionUbicacion;
+import aplicacion.domain.id.FactColeccionId;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,10 +13,20 @@ import lombok.Setter;
 @Setter
 @Entity
 public class FactColeccion {
-    @Id
-    private String idColeccion;
-    private Long idubicacion;
-    private Long idTiempo;
-    private Long idCategoria;
+    @EmbeddedId
+    private FactColeccionId id;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_coleccion")
+    private DimensionColeccion dimensionColeccion;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_ubicacion")
+    private DimensionUbicacion dimensionUbicacion;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_tiempo")
+    private DimensionTiempo dimensionTiempo;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_categoria")
+    private DimensionCategoria dimensionCategoria;
+
     private Long cantidadHechos;
 }
--- estadisticas/src/main/java/aplicacion/domain/facts/FactHecho.java
+++ estadisticas/src/main/java/aplicacion/domain/facts/FactHecho.java
@@ -1,7 +1,10 @@
 package aplicacion.domain.facts;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import aplicacion.domain.dimensiones.DimensionCategoria;
+import aplicacion.domain.dimensiones.DimensionTiempo;
+import aplicacion.domain.dimensiones.DimensionUbicacion;
+import aplicacion.domain.id.FactHechoId;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,9 +12,24 @@ import lombok.Setter;
 @Setter
 @Entity
 public class FactHecho {
-    @Id
-    private Long idUbicacion;
-    private Long idCategoria;
-    private Long idTiempo;
+
+    @EmbeddedId
+    private FactHechoId id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @MapsId("ubicacionId") // vincula con FactHechoId.ubicacionId
+    @JoinColumn(name = "id_ubicacion")
+    private DimensionUbicacion dimensionUbicacion;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @MapsId("tiempoId") // vincula con FactHechoId.tiempoId
+    @JoinColumn(name = "id_tiempo")
+    private DimensionTiempo dimensionTiempo;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @MapsId("categoriaId") // vincula con FactHechoId.categoriaId
+    @JoinColumn(name = "id_categoria")
+    private DimensionCategoria dimensionCategoria;
+
     private Long cantidadDeHechos;
 }
