0d54fa71346710dec08974e0c8237fc555f16729
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-09-18 19:30:13 -0300

    Repositorios y service

--- estadisticas/src/main/java/aplicacion/EstadisticasAplicacion.java
+++ estadisticas/src/main/java/aplicacion/EstadisticasAplicacion.java
@@ -2,14 +2,11 @@ package aplicacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "aplicacion.repositorios")
 public class EstadisticasAplicacion {
   public static void main(String[] args) {
-
     SpringApplication.run(EstadisticasAplicacion.class, args);
 
   }
---
+++ estadisticas/src/main/java/aplicacion/repositorios/DimensionTiempoRepository.java
@@ -0,0 +1,9 @@
+package aplicacion.repositorios;
+
+import aplicacion.domain.dimensiones.DimensionTiempo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DimensionTiempoRepository extends JpaRepository<DimensionTiempo, Long> {
+}
