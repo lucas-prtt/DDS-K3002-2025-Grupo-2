47ee90366545a54f3b6c3eb34b19637cf52e69af
nicopiacentini <npiacentini@frba.utn.edu.ar>
2025-09-19 11:46:48 -0300

    Repositorios

--- estadisticas/src/main/java/aplicacion/controllers/EstadisticasController.java
+++ estadisticas/src/main/java/aplicacion/controllers/EstadisticasController.java
@@ -1,8 +1,6 @@
 package aplicacion.controllers;
 
-import aplicacion.domain.dimensiones.DimensionCategoria;
-import aplicacion.repositorios.DimensionCategoriaRepository;
-import jakarta.annotation.PostConstruct;
+import aplicacion.repositorios.olap.DimensionCategoriaRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
