0856af4ff115b12e2913a5a8a7322e7f8dce41af
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-09-18 21:56:34 -0300

    Cachear provincias al leer el geoJson

--- estadisticas/src/main/java/aplicacion/EstadisticasAplicacion.java
+++ estadisticas/src/main/java/aplicacion/EstadisticasAplicacion.java
@@ -10,7 +10,6 @@ public class EstadisticasAplicacion {
   public static void main(String[] args) {
     SpringApplication.run(EstadisticasAplicacion.class, args);
     System.out.println(IdentificadorDeUbicacion.getInstance().identificar( -34.687719, -59.132967
-
     ));
   }
 }
