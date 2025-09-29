61710cf89ad74455f5ecdbff9f03fcbce3be5264
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-09-29 08:31:27 -0300

    Expando a peticion por paginas

--- estadisticas/src/main/java/aplicacion/controllers/EstadisticasController.java
+++ estadisticas/src/main/java/aplicacion/controllers/EstadisticasController.java
@@ -43,28 +43,41 @@ public class EstadisticasController {
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/provinciasDeColeccion")
-    public ResponseEntity<ProvinciaConMasHechosDeColeccionDTO> provinciasDeColeccion(@RequestParam("idColeccion") String idColeccion) {
-        return ResponseEntity.ok(estadisticasService.obtenerProvinciasConMasHechosDeUnaColeccion(idColeccion));
+    @GetMapping("/provinciasConMasHechosDeColeccion")
+    public ResponseEntity<List<ProvinciaConMasHechosDeColeccionDTO>> provinciasDeColeccion(@RequestParam("idColeccion") String idColeccion,@RequestParam(value = "page", defaultValue = "0") Integer page,@RequestParam(value = "limit", defaultValue = "1") Integer limit) {
+        if(validarPaginaYLimite(page, limit))
+            return ResponseEntity.badRequest().build();
+        return ResponseEntity.ok(estadisticasService.obtenerProvinciasConMasHechosDeUnaColeccion(idColeccion, page, limit));
     }
 
-    @GetMapping("/categoriaConMasDechosReportados")
-    public ResponseEntity<CategoriaConMasHechosDTO> categoriaConMasHechosReportados() {
-        return ResponseEntity.ok(estadisticasService.obtenerCategoriaConMasHechos());
+    @GetMapping("/categoriasConMasHechos")
+    public ResponseEntity<List<CategoriaConMasHechosDTO>> categoriaConMasHechosReportados(@RequestParam(value = "page", defaultValue = "0") Integer page,@RequestParam(value = "limit", defaultValue = "1") Integer limit) {
+        if(validarPaginaYLimite(page, limit))
+            return ResponseEntity.badRequest().build();
+        return ResponseEntity.ok(estadisticasService.obtenerCategoriaConMasHechos(page, limit));
     }
 
-    @GetMapping("/provinciaConMasHechosPorCategoria")
-    public ResponseEntity<ProvinciaConMasHechosDTO> provinciaConMasHechosDeCategoria(@RequestParam("nombreCategoria") String nombreCategoria) {
-        return ResponseEntity.ok(estadisticasService.obtenerProvinciaConMasHechosPorCategoria(nombreCategoria));
+    @GetMapping("/provinciasConMasHechosDeCategoria")
+    public ResponseEntity<List<ProvinciaConMasHechosDTO>> provinciaConMasHechosDeCategoria(@RequestParam("nombreCategoria") String nombreCategoria,@RequestParam(value = "page", defaultValue = "0") Integer page,@RequestParam(value = "limit", defaultValue = "1") Integer limit) {
+        if(validarPaginaYLimite(page, limit))
+            return ResponseEntity.badRequest().build();
+        return ResponseEntity.ok(estadisticasService.obtenerProvinciaConMasHechosPorCategoria(nombreCategoria, page, limit));
     }
 
-    @GetMapping("/horaMayorPorCategoria")
-    public ResponseEntity<HoraConMasHechosDeCategoriaDTO> horaConMasHechosDeCategoria(@RequestParam("nombreCategoria") String nombreCategoria) {
-        return ResponseEntity.ok(estadisticasService.obtenerHoraConMasHechosPorCategoria(nombreCategoria));
+    @GetMapping("/horaConMasHechosDeCategoria")
+    public ResponseEntity<List<HoraConMasHechosDeCategoriaDTO>> horaConMasHechosDeCategoria(@RequestParam("nombreCategoria") String nombreCategoria,@RequestParam(value = "page", defaultValue = "0") Integer page,@RequestParam(value = "limit", defaultValue = "1") Integer limit) {
+        if(validarPaginaYLimite(page, limit))
+            return ResponseEntity.badRequest().build();
+        return ResponseEntity.ok(estadisticasService.obtenerHoraConMasHechosPorCategoria(nombreCategoria, page, limit));
     }
 
     @GetMapping("/solicitudesDeEliminacionSpam")
     public ResponseEntity<CantidadSolicitudesSpamDTO> solicitudesSpam() {
         return ResponseEntity.ok(estadisticasService.obtenerCantidadSolicitudSpam());
     }
+
+    public boolean validarPaginaYLimite(Integer page, Integer limit){
+        return (page >= 0 && limit > 0);
+    }
+
 }
