119387a76af5722b80f63d1df5b92ec827660906
nicopiacentini <npiacentini@frba.utn.edu.ar>
2025-10-02 15:58:51 -0300

    Exportar en CSV - todos los endpoints

--- estadisticas/src/main/java/aplicacion/dtos/CategoriaConMasHechosDTO.java
+++ estadisticas/src/main/java/aplicacion/dtos/CategoriaConMasHechosDTO.java
@@ -1,11 +1,15 @@
 package aplicacion.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonPropertyOrder({ "nombreCategoria", "cantidadHechos"})
 public class CategoriaConMasHechosDTO {
+    @JsonIgnore
     private Long categoriaId;
     private String nombreCategoria;
     private Long cantidadHechos;
