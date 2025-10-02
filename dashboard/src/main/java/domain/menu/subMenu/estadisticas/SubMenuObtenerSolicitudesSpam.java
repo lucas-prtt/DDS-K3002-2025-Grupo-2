d204cbf44c49fae71f927fd61fe8f04d9b4c83a9
nicopiacentini <npiacentini@frba.utn.edu.ar>
2025-10-02 14:46:40 -0300

    BREAKING CHANGE: ahora csv funciona par uno de los endpoint

--- dashboard/src/main/java/domain/menu/subMenu/estadisticas/SubMenuObtenerSolicitudesSpam.java
+++ dashboard/src/main/java/domain/menu/subMenu/estadisticas/SubMenuObtenerSolicitudesSpam.java
@@ -7,7 +7,7 @@ import domain.dtos.estadisticasDTOs.CantidadSolicitudesSpamDTO;
 import java.util.Scanner;
 
 public class SubMenuObtenerSolicitudesSpam {
-    public static void abrirMenu(){
+    public static void abrirMenu() {
         CantidadSolicitudesSpamDTO cantidadSolicitudesSpamDTO = ApiClient.getSolicitudesDeEliminacionSpam(ConnectionManager.getInstance().getServidorLocal("Estadisticas"));
         System.out.println();
         System.out.println(cantidadSolicitudesSpamDTO);
