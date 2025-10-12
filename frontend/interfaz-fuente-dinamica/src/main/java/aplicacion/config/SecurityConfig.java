4f3abf3dc6390acd9767dad336711882d8d8a967
AgustinHerzkovich <aherzkovich@frba.utn.edu.ar>
2025-10-11 21:49:43 -0300

    Algunas reestructuraciones a nivel POM

---
+++ frontend/interfaz-fuente-dinamica/src/main/java/aplicacion/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package aplicacion.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/").permitAll() // La página principal es pública
+                        .anyRequest().authenticated() // Cualquier otra ruta requiere login
+                )
+                .oauth2Login(withDefaults()); // Habilita el flujo de login con OAuth2
+
+        return http.build();
+    }
+}
