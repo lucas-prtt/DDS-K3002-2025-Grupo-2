c5b1d445b62546ce29af18782388e83327ea79ae
AgustinHerzkovich <aherzkovich@frba.utn.edu.ar>
2025-10-11 21:49:43 -0300

    Algunas reestructuraciones a nivel POM

---
+++ frontend/interfaz-fuente-dinamica/src/main/java/aplicacion/InterfazFuenteDinamicaAplicacion.java
@@ -0,0 +1,14 @@
+package aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class InterfazFuenteDinamicaAplicacion {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InterfazFuenteDinamicaAplicacion.class, args);
+  }
+}
