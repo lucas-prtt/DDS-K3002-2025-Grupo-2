186be41eb4fb0c4aee7e388947065e59ee046de4
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-10-23 09:43:17 -0300

    Tests etiquetas en hecho y arreglo contieneEtiqueta

--- backend/agregador/src/main/test/testUnitarios/domain/hechos/HechoTest.java
+++ backend/agregador/src/main/test/testUnitarios/domain/hechos/HechoTest.java
@@ -1,12 +1,11 @@
 package testUnitarios.domain.hechos;
 
-import aplicacion.domain.hechos.Categoria;
-import aplicacion.domain.hechos.Hecho;
-import aplicacion.domain.hechos.Origen;
-import aplicacion.domain.hechos.Ubicacion;
+import aplicacion.domain.hechos.*;
 import aplicacion.domain.hechos.multimedias.Video;
 import aplicacion.domain.usuarios.Contribuyente;
 import aplicacion.domain.usuarios.IdentidadContribuyente;
+import domain.dashboardDTOs.hechos.HechoBuilder;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 import testUtils.HechoFactory;
@@ -18,8 +17,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HechoTest {
+    Hecho hecho = HechoFactory.crearHechoAleatorio();
+
     @Test
     public void sePuedeInstanciarUnHecho(){
         Assert.notNull(new Hecho("titulo", "descripcion", new Categoria("categoria"), new Ubicacion(2d, 2d), LocalDateTime.now(), Origen.DATASET, "contenidoTexto", List.of(new Video("mp4", 100, RandomThingsGenerator.generarLinkYoutubeAleatorio(), "720x1280", 2)), true, new Contribuyente(true, null, "mail")), "El hecho no se pudo crear");
+    }
+    @Test
+    public void generaClaveUnica(){
+        Assert.hasLength(hecho.getClaveUnica(), "Clave unica esta vacia");
+    }
+    @Test
+    public void iniciaSiendoVisible(){
+        Assert.isTrue(hecho.esVisible(), "No es visible");
+    }
+    @Test
+    public void seMuestraYOculta(){
+        Assert.isTrue(hecho.esVisible(), "No es visible");
+        hecho.ocultar();
+        Assert.isTrue(!hecho.esVisible(), "Es visible");
+        hecho.mostrar();
+        Assert.isTrue(hecho.esVisible(), "No es visible");
+        hecho.ocultar();
+        Assert.isTrue(!hecho.esVisible(), "Es visible");
+        hecho.mostrar();
+        Assert.isTrue(hecho.esVisible(), "No es visible");
+    }
+    @Test
+    public void esEtiquetable(){
+        hecho.etiquetar(new Etiqueta("Incendio"));
+        hecho.etiquetar(new Etiqueta("Catástrofe"));
+        Assert.isTrue(hecho.contieneEtiqueta(new Etiqueta("Incendio")), "No tiene etiquetas agregadas");
+        Assert.isTrue(!hecho.contieneEtiqueta(new Etiqueta("Catastrofe")), "No distingue tildes");
     }
 }
