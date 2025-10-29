9cffae5b4a8286eff21a43a34d22b6d3e9b54493
AgustinHerzkovich <aherzkovich@frba.utn.edu.ar>
2025-10-28 21:30:53 -0300

    arreglo la carga de hechos

--- backend/fuenteDinamica/src/main/java/aplicacion/dto/input/MultimediaInputDto.java
+++ backend/fuenteDinamica/src/main/java/aplicacion/dto/input/MultimediaInputDto.java
@@ -3,10 +3,14 @@ package aplicacion.dto.input;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class MultimediaInputDto {
     private String url;
 }
