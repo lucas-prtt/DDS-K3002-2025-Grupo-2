428587b3301793f55acb77b2bc26bb53a1280c2a
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-10-30 16:01:25 -0300

    Exceptionhandlers y gateway timeout 504 para si no anda el agregador

--- frontend/interfaz-agregador/src/main/java/aplicacion/controllers/GlobalExceptionHandler.java
+++ frontend/interfaz-agregador/src/main/java/aplicacion/controllers/GlobalExceptionHandler.java
@@ -1,15 +1,26 @@
 package aplicacion.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(WebClientException.class)
-    public String handleWebClientException(WebClientException ex) {
+    @ExceptionHandler(WebClientRequestException.class)
+    public String handleWebClientException(WebClientRequestException ex) {
         return "error/error-server-offline";
+    }
+
+    @ExceptionHandler(WebClientResponseException.class)
+    public String handleWebClientException(WebClientResponseException ex) {
+        if(ex.getStatusCode() == HttpStatus.GATEWAY_TIMEOUT) // 504, no se pudo conectar a agregador
+            return "error/error-server-offline";
+        else
+            return "error/500.html";
     }
 }
