d68aa5f2df12d0231e1edf4fcef4516993dd4212
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-11-01 11:49:33 -0300

    GET fuentes en FuenteEstatica

--- backend/fuenteEstatica/src/main/java/aplicacion/controllers/ArchivoController.java
+++ backend/fuenteEstatica/src/main/java/aplicacion/controllers/ArchivoController.java
@@ -1,18 +1,28 @@
 package aplicacion.controllers;
 
+import aplicacion.dto.output.FuenteOutputDTO;
 import aplicacion.services.ArchivoService;
+import aplicacion.services.AwsS3FileServerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/fuentesEstaticas")
 public class ArchivoController {
 
     private final ArchivoService archivoService;
-
-    public ArchivoController(ArchivoService archivoService) {
+    private final AwsS3FileServerService awsS3FileServerService;
+    public ArchivoController(ArchivoService archivoService, AwsS3FileServerService awsS3FileServerService) {
         this.archivoService = archivoService;
+        this.awsS3FileServerService = awsS3FileServerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FuenteOutputDTO>> getFuentesEstaticas(){
+        return ResponseEntity.ok(awsS3FileServerService.listarFuentes().stream().map(f -> new FuenteOutputDTO(f, awsS3FileServerService.listarArchivos("fuente" + f))).toList());
     }
 
     @PostMapping("/{id}/archivos/por-url")
