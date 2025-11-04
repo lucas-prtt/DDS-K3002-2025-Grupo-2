258efe1fb7a3ff7085a8ee029e1e7d5f9a3815e4
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-11-04 12:52:32 -0300

    Endpoint rename fuentes

---
+++ backend/agregador/src/main/java/aplicacion/dto/input/FuenteAliasDto.java
@@ -0,0 +1,14 @@
+package aplicacion.dto.input;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class FuenteAliasDto {
+    String alias;
+}
