a8a1d4162c484160b1ebdaecfa39770b2e31e9b4
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-11-05 08:52:50 -0300

    Mejora normalizador

--- backend/agregador/src/main/java/aplicacion/services/normalizador/NormalizadorDeHechos.java
+++ backend/agregador/src/main/java/aplicacion/services/normalizador/NormalizadorDeHechos.java
@@ -40,101 +40,28 @@ public class NormalizadorDeHechos {
     }
 
     public void normalizarTodos(Map<Fuente, List<Hecho>> mapFuentesYhechosANormalizar){
-        List<Long> tiempoPorPaso = new ArrayList<>();
-        tiempoPorPaso.add(0L); // Categorias
-        tiempoPorPaso.add(0L); // Etiquetas
         List<Hecho> hechos = mapFuentesYhechosANormalizar.values().stream()
                 .flatMap(List::stream)
                 .toList();
         System.out.println("Hechos a normalizar: " + mapFuentesYhechosANormalizar.values().stream().mapToInt(List::size).sum());
         System.out.println("Normalizando...");
         ProgressBar progressBar = new ProgressBar(hechos.size());
+        long inicio = System.nanoTime();
         for(Hecho hecho : hechos){
-            normalizarCronometrado(hecho, tiempoPorPaso);
+            normalizar(hecho);
             progressBar.avanzar();
         }
-        Long tiempoCategorias = tiempoPorPaso.get(0);
-        Long tiempoEtiquetas = tiempoPorPaso.get(1);
-        Long tiempoTotal = tiempoCategorias + tiempoEtiquetas;
+        long fin = System.nanoTime();
+        long tiempoTotal = fin - inicio;
         System.out.println("\nNormalización finalizada.\n");
         if(tiempoTotal != 0) {
-            System.out.println("          " + tiempoPorPaso.get(0) / 1_000_000 + " ms en normalizar Categorias" + "(" + (double) tiempoCategorias / tiempoTotal * 100 + " %)");
-            System.out.println("          " + tiempoPorPaso.get(1) / 1_000_000 + " ms en normalizar Etiquetas" + "(" + (double) tiempoEtiquetas / tiempoTotal * 100 + " %)");
+            System.out.printf("             %.2fs en normalizar (%.2f hechos/s)%n", (float) tiempoTotal / 1_000_000_000.0, (float) hechos.size() / (tiempoTotal / 1_000_000_000.0));
         }
-    }
-    public void normalizarMultiThread(Map<Fuente, List<Hecho>> mapFuentesYhechosANormalizar){
-        ExecutorService executor = Executors.newFixedThreadPool(3);
-        final ConcurrentHashMap<String, Object> locks =  new ConcurrentHashMap<>();
-        Object lockEtiqueta = locks.computeIfAbsent("etiqueta", k -> new Object());
-        Object lockCategoria = locks.computeIfAbsent("categoria", k -> new Object());
-        for(Hecho hecho : mapFuentesYhechosANormalizar.values().stream().flatMap(List::stream).toList())
-           executor.submit(() -> normalizarsincronizado(hecho, lockEtiqueta, lockCategoria));
-        executor.shutdown();
-        try {
-            if (!executor.awaitTermination(1, TimeUnit.HOURS)) { // Si no termina en una hora, fuerzo que se detenga
-                executor.shutdownNow();
-            }
-        } catch (InterruptedException e) {  // No debería pasar, pero por las dudas
-            executor.shutdownNow();
-            Thread.currentThread().interrupt();
-        }
-    }
-
-    private void normalizarsincronizado(Hecho hecho, Object lockEtiqueta, Object lockCategoria){
-        Categoria categoriaAInyectar;
-        List<Etiqueta> etiquetasAInyectar = new ArrayList<>();
-        synchronized (lockCategoria) {
-            String categoria = aplicarNormalizador(hecho.getCategoria().getNombre(), normalizadorDeCategorias);
-            try {
-                categoriaAInyectar = categoriaService.obtenerCategoriaPorNombre(categoria);
-            } catch (CategoriaNoEncontradaException e) {
-                categoriaAInyectar = categoriaService.agregarCategoria(categoria);
-            }
-        }
-        hecho.setCategoria(categoriaAInyectar);
-        Etiqueta etiquetaAInyectar;
-        synchronized (lockEtiqueta){
-            List<String> etiquetas = hecho.getEtiquetas().stream().map(Etiqueta::getNombre).map(n->aplicarNormalizador(n, normalizadorDeEtiquetas)).toList();
-            for(String nombre : etiquetas){
-                try{
-                    etiquetaAInyectar = etiquetaService.obtenerEtiquetaPorNombre(nombre);
-                } catch (EtiquetaNoEncontradaException e){
-                    etiquetaAInyectar = etiquetaService.agregarEtiqueta(nombre);
-                }
-                etiquetasAInyectar.add(etiquetaAInyectar);
-            }
-        }
-        hecho.setEtiquetas(etiquetasAInyectar);
     }
 
     public void normalizar(Hecho hecho)  {
         Categoria categoriaAInyectar;
         List<Etiqueta> etiquetasAInyectar = new ArrayList<>();
-        String categoria = aplicarNormalizador(hecho.getCategoria().getNombre(), normalizadorDeCategorias);
-        try{
-            categoriaAInyectar = categoriaService.obtenerCategoriaPorNombre(categoria);
-        }catch (CategoriaNoEncontradaException e){
-            categoriaAInyectar = categoriaService.agregarCategoria(categoria);
-        }
-        hecho.setCategoria(categoriaAInyectar);
-
-        List<String> etiquetas = hecho.getEtiquetas().stream().map(Etiqueta::getNombre).map(n->aplicarNormalizador(n, normalizadorDeEtiquetas)).toList();
-        Etiqueta etiquetaAInyectar;
-        for(String nombre : etiquetas){
-            try{
-                etiquetaAInyectar = etiquetaService.obtenerEtiquetaPorNombre(nombre);
-            } catch (EtiquetaNoEncontradaException e){
-                etiquetaAInyectar = etiquetaService.agregarEtiqueta(nombre);
-            }
-            etiquetasAInyectar.add(etiquetaAInyectar);
-        }
-        hecho.setEtiquetas(etiquetasAInyectar);
-    }
-
-    public void normalizarCronometrado(Hecho hecho, List<Long> segundosPorPaso)  {
-        Categoria categoriaAInyectar;
-        List<Etiqueta> etiquetasAInyectar = new ArrayList<>();
-        long inicioCat = System.nanoTime();
         String categoria = aplicarNormalizador(hecho.getCategoria().getNombre(), normalizadorDeCategorias);
         categoriaAInyectar = categoriaCache.getIfPresent(categoria);
         if(categoriaAInyectar == null){
@@ -146,8 +73,6 @@ public class NormalizadorDeHechos {
             categoriaCache.put(categoria, categoriaAInyectar);
         }
         hecho.setCategoria(categoriaAInyectar);
-        long finCat = System.nanoTime();
-
         List<String> etiquetas = hecho.getEtiquetas().stream().map(Etiqueta::getNombre).map(n->aplicarNormalizador(n, normalizadorDeEtiquetas)).toList();
         Etiqueta etiquetaAInyectar;
         for(String nombre : etiquetas){
@@ -163,9 +88,6 @@ public class NormalizadorDeHechos {
             etiquetasAInyectar.add(etiquetaAInyectar);
         }
         hecho.setEtiquetas(etiquetasAInyectar);
-        long finEt = System.nanoTime();
-        segundosPorPaso.set(0, segundosPorPaso.get(0) + finCat - inicioCat);
-        segundosPorPaso.set(1, segundosPorPaso.get(1) + finEt - finCat);
     }
     public String normalizarCategoria(String categoria) {
         return aplicarNormalizador(categoria, normalizadorDeCategorias);
