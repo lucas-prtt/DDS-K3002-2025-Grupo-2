08de4c46420914bf0230452e96af57228ddbed86
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-11-28 18:11:27 -0300

    Agrego mutex para agregar hechosXcoleccion y carga de hechos por scheduler
    El mutex se maneja a nivel fuente

--- backend/agregador/src/main/java/aplicacion/services/schedulers/CargarHechosScheduler.java
+++ backend/agregador/src/main/java/aplicacion/services/schedulers/CargarHechosScheduler.java
@@ -4,10 +4,7 @@ import aplicacion.domain.colecciones.Coleccion;
 import aplicacion.clasesIntermedias.HechoXColeccion;
 import aplicacion.domain.colecciones.fuentes.Fuente;
 import aplicacion.domain.hechos.Hecho;
-import aplicacion.services.ColeccionService;
-import aplicacion.services.DescubrirFuentesService;
-import aplicacion.services.FuenteService;
-import aplicacion.services.HechoService;
+import aplicacion.services.*;
 import aplicacion.services.depurador.DepuradorDeHechos;
 import aplicacion.services.normalizador.NormalizadorDeHechos;
 import aplicacion.utils.ProgressBar;
@@ -31,16 +28,17 @@ public class CargarHechosScheduler {
     private final DepuradorDeHechos depuradorDeHechos;
     private final HechoService hechoService;
     private final DescubrirFuentesService descubrirFuentesService;
-
+    private final FuenteMutexManager fuenteMutexManager;
     @Value("${hechos.lazy-loading}")
     boolean hechosSeCarganSoloSiEstanEnUnaColeccion;
-    public CargarHechosScheduler(FuenteService fuenteService, ColeccionService coleccionService, NormalizadorDeHechos normalizadorDeHechos, DepuradorDeHechos depuradorDeHechos, HechoService hechoService, DescubrirFuentesService descubrirFuentesService) {
+    public CargarHechosScheduler(FuenteService fuenteService, ColeccionService coleccionService, NormalizadorDeHechos normalizadorDeHechos, DepuradorDeHechos depuradorDeHechos, HechoService hechoService, DescubrirFuentesService descubrirFuentesService, FuenteMutexManager fuenteMutexManager) {
         this.fuenteService = fuenteService;
         this.coleccionService = coleccionService;
         this.normalizadorDeHechos = normalizadorDeHechos;
         this.depuradorDeHechos = depuradorDeHechos;
         this.hechoService = hechoService;
         this.descubrirFuentesService = descubrirFuentesService;
+        this.fuenteMutexManager = fuenteMutexManager;
     }
 
     @Scheduled(initialDelay = 30000, fixedRate = 3600000) // Se ejecuta cada 1 hora
@@ -62,13 +60,16 @@ public class CargarHechosScheduler {
             fuenteSet.addAll(fuenteService.obtenerTodasLasFuentes());
         }
         fuenteSet = fuenteSet.stream().filter(fuente -> fuente.getConexion().isOnlineUltimaVez()).collect(Collectors.toSet());
-        System.out.println("Se normalizaran " + fuenteSet.size() + " fuentes");
+        fuenteMutexManager.lockAll(fuenteSet.stream().map(Fuente::getId).toList());
+        try {
 
-        Map<Fuente, List<Hecho>> hechosPorFuente = fuenteService.hechosUltimaPeticion(fuenteSet.stream().toList());
-        normalizadorDeHechos.normalizarTodos(hechosPorFuente);
-        depuradorDeHechos.depurar(hechosPorFuente); // Depura hechos repetidos
+            System.out.println("Se normalizaran " + fuenteSet.size() + " fuentes");
 
-        System.out.println("""
+            Map<Fuente, List<Hecho>> hechosPorFuente = fuenteService.hechosUltimaPeticion(fuenteSet.stream().toList());
+            normalizadorDeHechos.normalizarTodos(hechosPorFuente);
+            depuradorDeHechos.depurar(hechosPorFuente); // Depura hechos repetidos
+
+            System.out.println("""
         
         
         ============================
@@ -79,36 +80,40 @@ public class CargarHechosScheduler {
         Se asignaran los hechos a las colecciones...
         
         """);
-        Long inicioAsignacion = System.nanoTime();
-        int indiceColeccion = 0;
-        int indiceFuente = 0;
-        for(Coleccion coleccion : colecciones){
-            indiceColeccion++;
-            System.out.println("Coleccion: " + indiceColeccion + " / " + colecciones.size());
+            Long inicioAsignacion = System.nanoTime();
+            int indiceColeccion = 0;
+            int indiceFuente = 0;
+            for(Coleccion coleccion : colecciones){
+                indiceColeccion++;
+                System.out.println("Coleccion: " + indiceColeccion + " / " + colecciones.size());
 
-            for(Fuente fuente : coleccion.getFuentes().stream().filter(fuente -> fuente.getConexion().isOnlineUltimaVez()).toList()){
-                indiceFuente++;
-                List<Hecho> hechosObtenidos = hechosPorFuente.get(fuente);
-                ProgressBar progressBar = new ProgressBar(hechosObtenidos.size(), "Fuente: "+indiceFuente+" / " + coleccion.getFuentes().size());
-                // hechosObtenidos = hechosObtenidos.stream.filter(hecho->hecho.noEstaPresente).toList();
-                for (Hecho hecho : hechosObtenidos) {
-                    HechoXColeccion hechoPorColeccion = new HechoXColeccion(hecho, coleccion);
-                    hechoService.guardarHechoPorColeccion(hechoPorColeccion);
-                    progressBar.avanzar();
+                for(Fuente fuente : coleccion.getFuentes().stream().filter(fuente -> fuente.getConexion().isOnlineUltimaVez()).toList()){
+                    indiceFuente++;
+                    List<Hecho> hechosObtenidos = hechosPorFuente.get(fuente);
+                    ProgressBar progressBar = new ProgressBar(hechosObtenidos.size(), "Fuente: "+indiceFuente+" / " + coleccion.getFuentes().size());
+                    // hechosObtenidos = hechosObtenidos.stream.filter(hecho->hecho.noEstaPresente).toList();
+                    for (Hecho hecho : hechosObtenidos) {
+                        HechoXColeccion hechoPorColeccion = new HechoXColeccion(hecho, coleccion);
+                        hechoService.guardarHechoPorColeccion(hechoPorColeccion);
+                        progressBar.avanzar();
+                    }
                 }
+                indiceFuente = 0;
             }
-            indiceFuente = 0;
-        }
-        Long finAsignacion = System.nanoTime();
-        System.out.printf("\nSe asignaron los hechos a las colecciones ( %2d ms )\n", (finAsignacion - inicioAsignacion)/1_000_000);
+            Long finAsignacion = System.nanoTime();
+            System.out.printf("\nSe asignaron los hechos a las colecciones ( %2d ms )\n", (finAsignacion - inicioAsignacion)/1_000_000);
 
-        // abrir map de fuente y lista de hechos, por cada fuente (fuente1, fuente2, ...) cargamos los hechos
-        // for (fuente) {fuente.hechos.cargarHechos()} en el metodo que haga esa carga de hechos se hace la validacion de si el hecho ya existe en bd (mediante equals)
-        // si ya existe no se carga el hecho pero se carga una entrada en HechoXFuente que asocie esta fuente y el hecho que ya existia
-        // si no existe se carga el hecho y se carga la entrada en HechoXFuente
-        // si se duplica un hecho dentro de la misma fuente -> Se sobreescribe? Presuponemos que no hay hechos duplicados dentro de una misma fuente
-        // en ambos casos se carga la entrada en hechoxfuente, lo que varia es a que hecho apunta.
-        // DECISION DE DISEÑO: si un hecho esta duplicado, conservamos el que estaba antes en la base de datos y descartamos el nuevo.
+            // abrir map de fuente y lista de hechos, por cada fuente (fuente1, fuente2, ...) cargamos los hechos
+            // for (fuente) {fuente.hechos.cargarHechos()} en el metodo que haga esa carga de hechos se hace la validacion de si el hecho ya existe en bd (mediante equals)
+            // si ya existe no se carga el hecho pero se carga una entrada en HechoXFuente que asocie esta fuente y el hecho que ya existia
+            // si no existe se carga el hecho y se carga la entrada en HechoXFuente
+            // si se duplica un hecho dentro de la misma fuente -> Se sobreescribe? Presuponemos que no hay hechos duplicados dentro de una misma fuente
+            // en ambos casos se carga la entrada en hechoxfuente, lo que varia es a que hecho apunta.
+            // DECISION DE DISEÑO: si un hecho esta duplicado, conservamos el que estaba antes en la base de datos y descartamos el nuevo.
+        }catch (Exception e){
+            System.err.println("No se pudo concretar la asignacion de hechos scheduleada");
+            e.printStackTrace();
+        }
         System.out.println("""
                 
                 =================================
