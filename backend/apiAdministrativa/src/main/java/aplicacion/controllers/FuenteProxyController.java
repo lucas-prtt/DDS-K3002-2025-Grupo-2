cb34138bbb215c528cd0d02bd224d4007c1dee4e
AgustinHerzkovich <aherzkovich@frba.utn.edu.ar>
2025-12-01 10:31:23 -0300

    agrego que se puedan cargar fuentes proxy desde apiAdministrativa

---
+++ backend/apiAdministrativa/src/main/java/aplicacion/controllers/FuenteProxyController.java
@@ -0,0 +1,27 @@
+package aplicacion.controllers;
+
+import aplicacion.config.ConfigService;
+import domain.peticiones.SolicitudesHttp;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/apiAdministrativa")
+public class FuenteProxyController {
+    private final String urlBaseProxy;
+    private final SolicitudesHttp solicitudesHttp;
+
+    public FuenteProxyController(ConfigService configService) {
+        this.urlBaseProxy = configService.getUrlFuentesProxy();
+        this.solicitudesHttp = new SolicitudesHttp(new RestTemplateBuilder());
+    }
+
+    @PostMapping("/fuentesProxy")
+    public ResponseEntity<Object> guardarFuente(@RequestBody String body){
+        return solicitudesHttp.post(urlBaseProxy + "/fuentesProxy", body, Object.class);
+    }
+}
