7f61b80b1dfed83b74875423074b75f17b4fba40
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-12-04 13:16:12 -0300

    Arreglo sliders exportar

--- frontend/interfaz-agregador/src/main/resources/static/js/estadisticas-exportar.js
+++ frontend/interfaz-agregador/src/main/resources/static/js/estadisticas-exportar.js
@@ -5,45 +5,45 @@ document.addEventListener("DOMContentLoaded", () => {
     let limit2 = 10;
     let limit3 = 10;
     let limit4 = 10;
-    function configurarSlider(sliderId, valorSpanId, callback) {
+    function configurarSlider(sliderId, valorSpanId, callback, maximo, exponente, divisor) {
         const slider = document.getElementById(sliderId);
         const valorSpan = document.getElementById(valorSpanId);
 
         function actualizar() {
-            const valorCuadratico = Math.ceil(Math.pow(slider.value, 2)/10);
+            const valorCuadratico = Math.ceil(Math.pow(slider.value, exponente)/divisor);
             valorSpan.textContent = valorCuadratico;
 
             const porcentaje = (slider.value - slider.min) / (slider.max - slider.min) * 100;
             slider.style.background = `linear-gradient(to right, #42a5f5 0%, #42a5f5 ${porcentaje}%, #ccc ${porcentaje}%, #ccc 100%)`;
 
-            if(slider.value == 100)
+            if(slider.value == maximo)
                 valorSpan.textContent = "todas las"
-            if (callback) callback((slider.value == 100? 2000000000 : valorCuadratico)); // Peak de la programacion
+            if (callback) callback((slider.value == maximo? 2000000000 : valorCuadratico)); // Peak de la programacion
         }
 
         slider.addEventListener("input", actualizar);
-        actualizar(); // valor inicial
+        actualizar();
     }
 
     // Hechos por provincia
     configurarSlider("slider-hechos-provincias", "valor-slider-hechos-provincias", (valor) => {
         limit1 = valor
-    });
+    }, 100, 2, 10);
 
     // Hechos por categoría
     configurarSlider("slider-hechos-categorias", "valor-slider-hechos-categorias", (valor) => {
         limit2=valor
-    });
+    }, 100, 2, 10);
 
     // Hechos por provincia y categoría
     configurarSlider("slider-hechos-provincia-categoria", "valor-slider-hechos-provincia-categoria", (valor) => {
         limit3=valor
-    });
+    }, 100, 2, 10);
 
     // Hora con más hechos por categoría
     configurarSlider("slider-hora-mas-hechos", "valor-slider-hora-mas-hechos", (valor) => {
         limit4=valor
-    });
+    }, 25, 1, 1); // 25 para no mostrar "todas" ni usar 2000000000 horas
 
     configurarDescargaCSV("btn-hechos-provincias", () => `http://localhost:8085/apiPublica/provinciasConMasHechosDeColeccion?limit=${limit1}`, "hechos_provincias.csv");
     configurarDescargaCSV("btn-hechos-categorias", () => `http://localhost:8085/apiPublica/categoriasConMasHechos?limit=${limit2}`, "hechos_categorias.csv");
